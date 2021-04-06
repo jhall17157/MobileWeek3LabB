dfe9ce020f4cc8f36f2086f6854acb8abdf3e1f7
jhall41 <jhall41@my.wctc.edu>
2021-04-05 21:18:24 -0600

    Initial

---
+++ AlcAgeLab/AlcAgeLab/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AlcAgeLab
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public void Btn_Clicked (object sender, System.EventArgs e)
+        {
+            DateTime date = Inp_date.Date;
+            DateTime now = DateTime.Now;
+            TimeSpan age = now - date;
+            string days = age.ToString("dddd");
+            int difference = Int32.Parse(days);
+            int years = difference / 365;
+            Lbl_age.Text = $"Age: {years}";
+            if(years < 21)
+            {
+                int diff = 21 - years;
+                Lbl_status.Text = $"You have {diff} years until you are 21";
+            } else
+            {
+                Lbl_status.Text = $"You're legal";
+            }
+            
+        }
+    }
+}
